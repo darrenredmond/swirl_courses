6c922ad807905f71628f6701f4c612103846326e
Darren Redmond <c_dredmond@groupon.com>
2017-01-14 07:47:42 +0000

    Add the code for Basic Building Blocks.

---
+++ R_ProgrammingDR/Basic_Building_Blocks/customTests.R
@@ -0,0 +1,49 @@
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Get the value which a user either entered directly or was computed
+# by the command he or she entered.
+getVal <- function(){
+  getState()$val
+}
+
+# Get the last expression which the user entered at the R console.
+getExpr <- function(){
+  getState()$expr
+}
+
+library(digest)
+
+dbs_on_demand <- function(){
+  selection <- getState()$val
+  if(selection == "Yes"){
+    course <- "R: Basic Building Blocks"
+    email <- readline("What is your email address? ")
+    student_number <- readline("What is your student number? ")
+    hash <- digest(paste(course, student_number), "md5", serialize = FALSE)
+    digest("foo", "md5", serialize = FALSE)
+    
+    payload <- sprintf('{  
+      "course": "%s",
+      "email": "%s",  
+      "student_number": "%s",  
+      "hash": "%s",  
+    }', course, email, student_number, hash)
+    url <- 'http:///results.dbsdataprojects.com/course_results/submit'
+  
+    respone <- httr::POST(url, body = payload)
+    if(respone$status_code >= 200 && respone$status_code < 300){
+      message("Grade submission succeeded!")
+    } else {
+      message("Grade submission failed.")
+      message("Press ESC if you want to exit this lesson and you")
+      message("want to try to submit your grade at a later time.")
+      return(FALSE)
+    }
+  }
+  TRUE
+}
